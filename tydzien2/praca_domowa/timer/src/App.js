16508e93fefda6d3cf4fd154e97a3ea776458cd5
Jakub Lyko <jlyko@future-processing.com>
2022-04-08 15:02:42 +0200

    implemented App & TimeboxEditor

--- tydzien2/praca_domowa/timer/src/App.js
+++ tydzien2/praca_domowa/timer/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Timebox from './components/Timebox'
 import TimeboxEditor from './components/TimeboxEditor'
+import { useState } from 'react'
 
 function App() {
+  const [isRunning, setRunning] = useState(false)
+  
   return (
     <div className="App">
       <h1>Kurs Reacta Tydzień 2</h1>
       <hr></hr>
-      <TimeboxEditor />
-      <Timebox />
+      <TimeboxEditor isRunning setRunning />
+      <Timebox isRunning setRunning />
     </div>
   );
 }
--- tydzien2/praca_domowa/timer/src/components/TimeboxEditor/TimeboxEditor.jsx
+++ tydzien2/praca_domowa/timer/src/components/TimeboxEditor/TimeboxEditor.jsx
@@ -1,21 +1,39 @@
 import "./TimeboxEditor.css"
+import { useState } from 'react'
 
-export const TimeboxEditor = () => {
+export const TimeboxEditor = ({ isRunning, setRunning, setTitle, setTime }) => {
+  const [_title, _setTitle] = useState('')
+  const [_time, _setTime] = useState(0)
+
+  const handleOnChangeTitle = event => {
+    _setTitle(event.value)
+  }
+
+  const handleOnChangeTime = event => {
+    _setTime(event.value)
+  }
+
+  const handleButtonClick = event => {
+    setTitle(_title)
+    setTime(_time)
+    setRunning(true)
+  }
+
   return (
-    <div className="TimeboxEditor">
+    <div className={`TimeboxEditor ${isRunning ? 'inactive' : ''}`}>
       <label>
         Co robisz?
-        <input type="text" value="Uczę się skrótów klawiszowych"></input>
+        <input disabled={isRunning} type="text" value={_title} onChange={handleOnChangeTitle}></input>
         <br />
       </label>
 
       <label>
         Ile minut?
-        <input type="number" value="20"></input>
+        <input disabled={isRunning} type="number" value={_time} onChange={handleOnChangeTime}></input>
         <br />
       </label>
 
-      <button>Zacznij</button>
+      <button disabled={isRunning} onClick={handleButtonClick}>Zacznij</button>
     </div>
   )
 }
