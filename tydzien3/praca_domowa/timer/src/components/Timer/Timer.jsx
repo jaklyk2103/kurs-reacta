785b72cfee72c622540749e231ca6799dd6ef89a
Jakub Lyko <jlyko@future-processing.com>
2022-04-09 22:00:34 +0200

    WEEK-3 added timer changing feature

--- tydzien3/praca_domowa/timer/src/components/Timer/Timer.jsx
+++ tydzien3/praca_domowa/timer/src/components/Timer/Timer.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react'
+
 export const Timer = ({timer, onTimerDelete, onTimerChange}) => {
   const { name, time } = timer
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedName, setEditedName] = useState('')
+  const [editedTime, setEditedTime] = useState(0)
+
+  const handleChangeClick = () => {
+    setIsEditing(!isEditing)
+  }
+  const handleChangeSubmitClick = () => {
+    onTimerChange({key: timer.key, name: editedName, time: editedTime})
+  }
+  const handleEditedNameChange = event => {
+    setEditedName(event.target.value)
+  }
+  const handleEditedTimeChange = event => {
+    setEditedTime(event.target.value)
+  }
+
   return (
     <div className="Timer">
       <h1>{name} - {time} min.</h1>
       <button onClick={() => onTimerDelete(timer)}>Usuń</button>
-      <button onClick={() => onTimerChange({key: timer.key, name: 'xd', time: 123})}>Zmień</button>
+      <button onClick={handleChangeClick}>Zmień</button>
+      <div hidden={!isEditing}>
+        <input type="text" value={editedName} onChange={handleEditedNameChange} />
+        <input type="number" value={editedTime} onChange={handleEditedTimeChange} />
+        <button onClick={handleChangeSubmitClick}>Zatwierdź</button>
+      </div>
     </div>
   )
 }
