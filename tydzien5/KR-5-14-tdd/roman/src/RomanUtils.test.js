c6b1698f01f9f6c531b1e4e660dd86b50b6698e2
Jakub Lyko <jlyko@future-processing.com>
2022-05-18 20:20:23 +0200

    Added tests for converting 50 to L, 100 to C, 500 to D and 1000 to M for toRoman

--- tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
+++ tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
@@ -5,6 +5,10 @@ describe('toRoman', () => {
     [1, 'I'],
     [5, 'V'],
     [10, 'X'],
+    [50, 'L'],
+    [100, 'C'],
+    [500, 'D'],
+    [1000, 'M'],
   ])('Should convert %d to %s', (input, expectedOutput) => {
     expect(toRoman(input)).toBe(expectedOutput)
   })
