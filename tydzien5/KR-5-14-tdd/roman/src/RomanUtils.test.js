206847a5a80ffb40e20c991825f5af3a183adee8
Jakub Lyko <jlyko@future-processing.com>
2022-05-18 20:14:01 +0200

    Added test for converting 5 to V for toRoman

--- tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
+++ tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
@@ -3,6 +3,7 @@ import { toRoman } from './RomanUtils'
 describe('toRoman', () => {
   test.each([
     [1, 'I'],
+    [5, 'V'],
     [10, 'X'],
   ])('Should convert %d to %s', (input, expectedOutput) => {
     expect(toRoman(input)).toBe(expectedOutput)
