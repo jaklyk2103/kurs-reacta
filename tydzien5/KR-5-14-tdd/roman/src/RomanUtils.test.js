29e00a73a32d7a010c4eb3889e55e620c9736483
Jakub Lyko <jlyko@future-processing.com>
2022-05-18 20:25:40 +0200

    Added new group of tests for testing arabic to roman values others than single symbols

--- tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
+++ tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
@@ -1,6 +1,6 @@
 import { toRoman } from './RomanUtils'
 
-describe('toRoman', () => {
+describe('toRoman symbols convertion', () => {
   test.each([
     [1, 'I'],
     [5, 'V'],
@@ -9,6 +9,14 @@ describe('toRoman', () => {
     [100, 'C'],
     [500, 'D'],
     [1000, 'M'],
+  ])('Should convert %d to %s', (input, expectedOutput) => {
+    expect(toRoman(input)).toBe(expectedOutput)
+  })
+})
+
+describe('toRoman complex values convertion ', () => {
+  test.each([
+    [6, 'VI'],
   ])('Should convert %d to %s', (input, expectedOutput) => {
     expect(toRoman(input)).toBe(expectedOutput)
   })
