230ef62af2af6665a16e4b6b8ac85c73a8c52508
Jakub Lyko <jlyko@future-processing.com>
2022-05-18 20:13:20 +0200

    Refactored existing toRoman tests to test.each()

--- tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
+++ tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
@@ -1,10 +1,10 @@
 import { toRoman } from './RomanUtils'
 
 describe('toRoman', () => {
-  test('Should convert 1 to I', () => {
-    expect(toRoman(1)).toBe('I')
-  })
-  test('Should convert 10 to X', () => {
-    expect(toRoman(10)).toBe('X')
+  test.each([
+    [1, 'I'],
+    [10, 'X'],
+  ])('Should convert %d to %s', (input, expectedOutput) => {
+    expect(toRoman(input)).toBe(expectedOutput)
   })
 })
