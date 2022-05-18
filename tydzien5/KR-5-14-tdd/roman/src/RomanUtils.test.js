50f35088bf66a6910dfa6bff3452200d499ca36e
Jakub Lyko <jlyko@future-processing.com>
2022-05-18 20:10:38 +0200

    Added test for converting 10 to X for toRoman

--- tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
+++ tydzien5/KR-5-14-tdd/roman/src/RomanUtils.test.js
@@ -4,4 +4,7 @@ describe('toRoman', () => {
   test('Should convert 1 to I', () => {
     expect(toRoman(1)).toBe('I')
   })
+  test('Should convert 10 to X', () => {
+    expect(toRoman(10)).toBe('X')
+  })
 })
